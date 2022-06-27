472d2c785d6b321e03a6027ab04747e52492e03e
李万元 <449736038@qq.com>
2022-06-27 11:02:16 +0800

    优化重复调用逻辑

--- db/db.go
+++ db/db.go
@@ -10,7 +10,13 @@ import (
 	"time"
 )
 
-func Connect(dsn string) (db *gorm.DB, err error) {
+var db *gorm.DB
+
+func Connect(dsn string) (*gorm.DB, error) {
+	var err error
+	if db != nil {
+		return db, nil
+	}
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
